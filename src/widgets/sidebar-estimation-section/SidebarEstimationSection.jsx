9073d3abf2aeb28d10c7a1a9bc195fd3d153ea25
daniil <daniil@clean.email>
2020-01-31 19:11:51 +0200

    remove extra github classes

--- src/widgets/sidebar-estimation-section/SidebarEstimationSection.jsx
+++ src/widgets/sidebar-estimation-section/SidebarEstimationSection.jsx
@@ -12,18 +12,16 @@ export default class SidebarEstimationSection extends React.PureComponent {
 	render () {
 		return (
 			<form className="issue-sidebar-form" aria-label="Make estimations">
-				<div className="details-reset details-overlay select-menu hx_rsm">
-					<div className="text-bold discussion-sidebar-heading discussion-sidebar-toggle hx_rsm-trigger" aria-haspopup="menu" data-hotkey="a" role="button">
-						<Icon name="trash" className="octicon" />
-						Estimations
-					</div>
+				<div className="issue-sidebar-heading">
+					<Icon name="trash" className="octicon" />
+					Estimations
 				</div>
 
 				<Label className="estimation-field">
 					<Input />
 				</Label>
-				<Button className="btn-sm btn-primary" onClick={this.handleClick} type="submit">Save</Button>
-				<Button className="btn-sm">Cancel</Button>
+				<Button className="primary-button button" onClick={this.handleClick} type="submit">Save</Button>
+				<Button className="secondary-button button">Cancel</Button>
 			</form>
 		);
 	}
